790b8f1edd5e43741437febb769b89bf53d01edf
larobben13 <89539114+larobben13@users.noreply.github.com>
2021-09-20 23:20:42 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,59 @@
+//Question 1 myNumber Variable 
+let myNumber; 
+console.log(myNumber = 5); //Question 2 myNumber equal to 5
+
+//Question 3 myOtherNumber variable
+//myOtherNumber equal to 5
+let myOtherNumber; 
+console.log(myOtherNumber = 5);
+
+//Question 4 myTotal Variable
+//myTotal vaiable equal to the product of myNumber and myOtherNumber
+let myTotal; 
+console.log(myTotal = myNumber * myOtherNumber); 
+
+//Question 5 myName Variable
+//The variable myName should by defined.
+//The variable myName should be type string.
+const myName = "Lauren"
+console.log("My first name is " + myName);
+//The variable myCombo should by defined.
+//The variable myCombo should be type string.
+//The variable myCombo should be equal to the sum of myNumber and myOtherNumber
+const myCombo = myNumber + myOtherNumber;
+console.log("Your combination of numbers is " + myCombo);
+
+//Question 6
+//The variable myOtherCombo should by defined.
+//The variable myOtherCombo should be type number.
+//The variable myOtherCombo should be equal to the sum of myNumber and myOtherNumber.‣
+let myOtherCombo;
+console.log(myNumber + myOtherNumber)
+
+//Question 7 
+//The variable myRemainder should by defined.
+//The variable myRemainder should be type number.
+//The variable myRemainder should be equal to the remainder of myNumber divided by 5.
+let myRemainder;
+console.log(myRemainder = myNumber / 5);
+
+//Question 8 
+//The variable i should by defined.
+//The variable i should be type number.
+//The variable i should be equal to 2
+let i = 1;
+console.log(i + 1);
+
+//Question 9
+//The variable int should by defined.
+//The variable int should be type number.
+//The variable int should be equal to 4.
+const number = 4;
+console.log(Number.isInteger(number));
+
+//Question 10
+//The variable myBoolean should by defined.
+//The variable myBoolean should be type boolean.
+//The variable myBoolean should be false/.
+let myBoolean;
+console.log(3 < 2)
